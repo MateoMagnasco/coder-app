77acefac68e9462f9fc118dd48d11db398d600b4
Mateo Magnasco <magnasco.mateo@gmail.com>
2021-09-15 13:42:19 -0300

    renombre archivo

--- src/App.js
+++ src/App.js
@@ -44,34 +44,13 @@ function App() {
   
     <div className="App">
       <header className="App-header">
-      
         {view? <NavBar/>: <h1>Se oculto el NavBar</h1>}<hr/>
-        
-        
         <h1>Bienvenidos a mi tienda de Guitarras!</h1>
-      
         <button onClick={()=>setView(!view)}>Mostrar navbar?</button>
          {/* <h1 >{count}</h1> */}
-         </header>
-         
-        
+      </header>
         <ItemListContainer/>
         <ItemDetailContainer/>
-        
-        
-        {/* <button onClick={() => setCount(count+1)}>Añadir al carrito</button>
-        <button onClick ={miFuncionRestar}> Sacar del carrito</button>
-        {count <= 0 && <h3>El contador es 0!</h3> }  */}
-        
-        
-        
-        
-        
-
-        
-        
-        
-      
     </div>
   );
 }
